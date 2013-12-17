5223a6cc7d5b0f1440027a1f8bf8480b96dbeb79
Rafael Erthal <rafaerthal@gmail.com>
2013-12-17 21:01:56 -0200

    fixing popup

--- angular/controllers/newsletter.js
+++ angular/controllers/newsletter.js
@@ -1,17 +1,26 @@
 /* global angular:false
  */
-angular.module('galatea.controllers.newsletter', ['resources']).config(function () {
+angular.module('galatea.controllers.newsletter', ['resources', 'ngCookies']).config(function () {
     'use strict';
 
-}).controller('NewsletterController', function ($scope, newsletter) {
+}).controller('NewsletterController', function ($scope, $rootScope, $cookieStore, newsletter) {
     'use strict';
 
     $scope.newsletter = new newsletter();
+
+    $rootScope.popUp = !$cookieStore.get('newsletter');
+
+    $scope.close = function () {
+        $cookieStore.put('newsletter', true);
+        $rootScope.popUp = false;
+    };
 
     $scope.save = function () {
         $scope.newsletter.$save(function () {
             $scope.success = 'Obrigado por ser inscrever em nossa newsletter!!';
             $scope.newsletter = new newsletter();
         });
+        $cookieStore.put('newsletter', true);
+        $rootScope.popUp = false;
     };
 });
